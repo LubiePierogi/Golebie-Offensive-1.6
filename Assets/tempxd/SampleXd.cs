ad14e795e5aed8bb27e9ff8411b5e7137b84b7c3
LubiePierogi <26669430+LubiePierogi@users.noreply.github.com>
2019-05-10 22:40:59 +0200

    skrypty xd

---
+++ Assets/tempxd/SampleXd.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SampleXd : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("jabłka są baaardzo smaczne xd");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
