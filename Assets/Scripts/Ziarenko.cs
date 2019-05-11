4d06a3686f663fbafabbabf139bd73752053873e
LubiePierogi <26669430+LubiePierogi@users.noreply.github.com>
2019-05-11 10:38:33 +0200

    sdokodso

---
+++ Assets/Scripts/Ziarenko.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ziarenko : MonoBehaviour
+{
+    // Type - 0 - green, 1 - purple 
+    public int type = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        /*
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+        switch (type)
+        {
+            case 0:
+                break;
+            case 1: default:
+                sr.sprite.
+           */
+        //}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
